420463d56dc465090c00e5feaf1e9895b5339af1
shashwat283 <shashwatrajwanshi41@gmail.com>
2025-05-19 23:50:30 +0530

    started

---
+++ formvalidation/formvalidation/formvalidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  formvalidationApp.swift
+//  formvalidation
+//
+//  Created by shashwat singh on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct formvalidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
